30e3f8d382633b5cf1ba60a31654255f6701d6f3
prashantS95 <106652585+prashantS95@users.noreply.github.com>
2024-11-28 12:37:34 +0530

    Add files via upload
    string formatting methods

---
+++ string.py
@@ -0,0 +1,4 @@
+
+wickets= int(input("Enter wickets "))
+print("Hopefully india doesnt loost by {} wickets remaining this time against pak ".format(wickets))
+print(f"Hopefully india doesnt loost by {wickets} wickets remaining this time against pak ")
